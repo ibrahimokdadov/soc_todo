82a2e2e8c884b3c55274c2f7f22a91ac8bcfa988
ibininja <ibrahim.mokdad@gmail.com>
2015-12-18 13:19:56 +0300

    First push. Containing structure and part implemntation of categories

---
+++ src/common/database.py
@@ -0,0 +1,37 @@
+import pymongo
+
+__author__ = 'team_project_2015'
+
+
+class Database(object):
+    URI = "mongodb://127.0.0.1:27017"
+    DB = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DB = client['movies_library']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DB[collection].insert(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DB[collection].find(query)
+
+    @staticmethod
+    def find_all(collection):
+        return Database.DB[collection].find()
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DB[collection].find_one(query)
+
+    @staticmethod
+    def update_one(collection, query, setquery):
+        return Database.DB[collection].update(query, setquery)
+
+    @staticmethod
+    def remove(collection, query):
+        return Database.DB[collection].remove(query)
