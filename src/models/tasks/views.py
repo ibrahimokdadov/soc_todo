29fd202cdc9e393050455ba2dae94c6f815069a2
ibininja <ibrahim.mokdad@gmail.com>
2016-01-25 10:32:55 +0300

    added ability to view task details and edit task information

--- src/models/tasks/views.py
+++ src/models/tasks/views.py
@@ -17,7 +17,7 @@ def view_tasks(folder_id):
     else:
         if (session.get('productivity') is not None) and (session['productivity'] == 1):
             tasks = Task.get_future_three_tasks(folder_id)
-            #TODO: display passed undone tasks
+            # TODO: display passed undone tasks
             if tasks is not None:
                 return render_template("tasks/list_tasks.html", folder_id=folder_id, tasks=tasks)
             else:
@@ -29,6 +29,7 @@ def view_tasks(folder_id):
             else:
                 return render_template("tasks/list_tasks.html")
 
+
 @task_blueprints.route('/user/folder/tasks/expired/<string:folder_id>')
 def view_expired_tasks(folder_id):
     if session.get('email') is None:
@@ -36,7 +37,7 @@ def view_expired_tasks(folder_id):
     else:
         if (session.get('productivity') is not None) and (session['productivity'] == 1):
             tasks = Task.get_previous_tasks(folder_id)
-            #TODO: display passed undone tasks
+            # TODO: display passed undone tasks
             if tasks is not None:
                 return render_template("tasks/list_tasks.html", folder_id=folder_id, tasks=tasks)
             else:
@@ -64,9 +65,9 @@ def add_task(folder_id):
             folder.add_task(title=task_title, description=task_description, due_date=task_due_date)
             return make_response(view_tasks(folder_id))
 
+
 @task_blueprints.route('/user/task/done/<int:set>')
-def add_numbers(set):
-    print(set)
+def mark_done(set):
     if set is 0:
         task_id = request.args.get('task_id')
         task = Task.get_task_by_id(task_id)
@@ -92,8 +93,43 @@ def add_numbers(set):
         return jsonify(result=None)
 
 
+@task_blueprints.route('/user/folder/task/detail/<string:task_id>')
+def task_detail(task_id):
+    task = Task.get_task_by_id(task_id)
+    if task is not None:
+        return render_template("tasks/task_details.html", task=task)
+    else:
+        return render_template("tasks/task_details.html", task=task, message="Could not located task")
 
-#test method
+
+@task_blueprints.route('/user/folder/task/update/title', methods=['GET', 'POST'])
+def update_title():
+    task_id = request.form['pk']
+    value = request.form['value']
+    task = Task.get_task_by_id(task_id)
+    task.update_task("title", value)
+    return render_template("tasks/task_details.html", task=task)
+
+
+@task_blueprints.route('/user/folder/task/update/description', methods=['GET', 'POST'])
+def update_description():
+    task_id = request.form['pk']
+    value = request.form['value']
+    task = Task.get_task_by_id(task_id)
+    task.update_task("description", value)
+    return render_template("tasks/task_details.html", task=task)
+
+@task_blueprints.route('/user/folder/task/update/due_date', methods=['GET', 'POST'])
+def update_due_date():
+    task_id = request.form['pk']
+    value = request.form['value']
+    task_due_date= datetime.datetime.strptime(value, '%Y-%m-%d %H:%M')
+    task = Task.get_task_by_id(task_id)
+    task.update_task("due_date", task_due_date)
+    return render_template("tasks/task_details.html", task=task)
+
+
+# test method
 @task_blueprints.route('/user/productivity/one')
 def productivity_one():
     if session.get('email') is None:
@@ -101,7 +137,7 @@ def productivity_one():
     else:
         user = User.get_user_by_email(session['email'])
         folders = Folder.get_folders_by_user_id(user._id)
-        tasks_list=[]
+        tasks_list = []
         if folders is not None:
             for folder in folders:
                 tasks_list.append({folder: Task.get_future_three_tasks(folder._id)})
